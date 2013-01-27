d5d3f85d381de070fa4abbd7e991ba8c79a0923b
Cameron Paul <cpaul37@gmail.com>
2013-01-27 13:45:42 -0800

    Added hiccup to project

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.2"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler imaje.handler/app}
   :profiles
